5d5402ba1ba83e70363cc1b414167459370a7078
DonValen910 <valentinstito@gmail.com>
2025-12-23 16:07:54 -0300

    feat: agregar componente FlechaDerecha con SVG personalizado

---
+++ src/components/icons/FlechaDerecha.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FlechaDerecha = ({ tamaño, color }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={tamaño} height={tamaño} viewBox="0 0 24 24"><path fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m9 5l6 7l-6 7"/></svg>
+  )
+}
+
+export default FlechaDerecha
