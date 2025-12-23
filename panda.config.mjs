c852f90bdfc7662d668695f10eddc71be80d143e
DonValen910 <valentinstito@gmail.com>
2025-12-23 15:26:28 -0300

    feat: agregar configuración de estilos globales en panda.config.mjs y crear archivo globalCss.js

--- panda.config.mjs
+++ panda.config.mjs
@@ -10,6 +10,17 @@ export default defineConfig({
   // Files to exclude
   exclude: [],
 
+  // Global styles
+  globalCss: {
+    ':root': {
+      '--color-naranja': '#FF7E2D',
+      '--color-rojo': '#F00808',
+      '--color-verde': '#1AA113',
+      '--color-negro': '#010101',
+      '--color-blanco': '#F1F1F1',
+    }
+  },
+
   // Useful for theme customization
   theme: {
     extend: {},
