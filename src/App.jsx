395baa59ce17ef202fa8e3cf8053d24cfe1b9220
DonValen910 <valentinstito@gmail.com>
2025-12-24 02:27:32 -0300

    feat: agregar componente Chips y contenedor flexible en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { css } from '../styled-system/css'
 import Card from './components/Card.jsx'
+import Chips from './components/Chips.jsx'
+
+const ContainerChips = css({
+  display: 'flex',
+  justifyContent: 'center',
+  gap: '1rem',
+})
 
 function App() {
+  const fechas = ['01-08', '09-15', '16-22', '23-31'];
+
   return (
     <>
+      <div className={ContainerChips}>
+        {fechas.map((fecha, index) => (
+          <Chips key={index} fecha={fecha} />
+        ))}
+      </div>
       <Card TipoEstado={"Terminado"} Fecha={"14, Dic"} />
     </>
   )
