407559c0a61f3f345c8984cae9ece24827258d91
DonValen910 <valentinstito@gmail.com>
2026-01-31 17:11:35 -0300

    feat: mejorar la estructura del componente DayPage con estilos para ContenedorCard

--- src/pages/days/DayPage.jsx
+++ src/pages/days/DayPage.jsx
@@ -1,17 +1,24 @@
+import { css } from '../../../styled-system/css'
 import DayHeader from "./DayHeader";
 import DayFooter from "./DayFooter";
 import BotonFiltro from "./components/BotonFiltro";
 import ReservationCard from "./components/ReservationCard";
 
+const ContenedorCard = css({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '1rem'
+})
+
 export default function DayPage() {
-  return (
-    <>
-      <DayHeader TipoEstado="Terminado" Fecha="14, Dic" NumReservas={7} />
-      <BotonFiltro />
-      <main>
-        <ReservationCard nombre={'Matias'} numCancha={1} seña={8000} horaInicio={"17:00"} horaFin={"18:00"} />
-      </main>
-      <DayFooter />
-    </>
-  )
+    return (
+        <>
+            <DayHeader TipoEstado="Terminado" Fecha="14, Dic" NumReservas={7} />
+            <BotonFiltro />
+            <main className={ContenedorCard}>
+                <ReservationCard nombre={'Matias'} numCancha={1} seña={8000} horaInicio={"17:00"} horaFin={"18:00"} />
+            </main>
+            <DayFooter />
+        </>
+    )
 }
