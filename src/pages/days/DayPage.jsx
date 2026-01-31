b09ba58ea0013ea4f96c5b76c5ac7bb0f541caab
DonValen910 <valentinstito@gmail.com>
2026-01-31 15:04:25 -0300

    feat: implementar la página DayPage con componentes DayHeader, BotonFiltro y DayFooter

--- src/pages/days/DayPage.jsx
+++ src/pages/days/DayPage.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import DayHeader from "./DayHeader";
+import DayFooter from "./DayFooter";
+import BotonFiltro from "./components/BotonFiltro";
 
 export default function DayPage() {
   return (
-    <div>DayPage</div>
+    <>
+      <DayHeader TipoEstado="Terminado" Fecha="14, Dic" NumReservas={7} />
+      <BotonFiltro />
+      <DayFooter />
+    </>
   )
 }
