a9c0376c000aa8daaa69ddd5ad161deb5eaee763
DonValen910 <valentinstito@gmail.com>
2026-01-31 15:04:18 -0300

    feat: agregar componente DayFooter y mejorar estilos en DayHeader

---
+++ src/pages/days/DayFooter.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { css } from '../../../styled-system/css'
+import { useNavigate } from 'react-router'
+
+const FooterContenedor = css({
+    width: '100%',
+    display: 'flex',
+    fontFamily: 'var(--fuente-montserrat)',
+    fontWeight: 'bold',
+    fontSize: '30px',
+})
+
+const BotonAtras = css({
+    flex: 1,
+    padding: '10px 0',
+    backgroundColor: 'var(--color-blanco)',
+    cursor: 'pointer',
+})
+
+const BotonAñadir = css({
+    flex: 1,
+    padding: '10px 0',
+    backgroundColor: 'var(--color-verde)',
+    cursor: 'pointer',
+})
+
+export default function DayFooter() {
+	const navigate = useNavigate();
+
+    return (
+        <footer className={FooterContenedor}>
+            <button className={BotonAtras} onClick={() => navigate('/')}>Atras</button>
+            <button className={BotonAñadir}>Añadir</button>
+        </footer>
+    )
+}
--- src/pages/days/DayHeader.jsx
+++ src/pages/days/DayHeader.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
+import { css } from '../../../styled-system/css'
 
-export default function DayHeader() {
+const IndicadorEstadoBase = css({
+  width: '100%',
+  height: '80px',
+  display: 'grid',
+  placeItems: 'center',
+  fontSize: '24px',
+  fontWeight: 'bold',
+  fontFamily: 'var(--fuente-montserrat)',
+})
+
+export default function DayHeader({ TipoEstado, Fecha, NumReservas }) {
+  let estadoColor = '';
+
+  switch (TipoEstado) {
+    case "Pendiente":
+      estadoColor = css({
+        backgroundColor: 'var(--color-naranja)'
+      })
+      break;
+
+    case "Proximo":
+      estadoColor = css({
+        backgroundColor: 'var(--color-rojo)',
+      })
+      break;
+
+    case "Terminado":
+      estadoColor = css({
+        backgroundColor: 'var(--color-verde)',
+      })
+      break;
+
+    default:
+      console.log("Tipo de estado no reconocido");
+      break;
+  }
+
+  const IndicadorConEstado = `${IndicadorEstadoBase} ${estadoColor}`;
+  const Texto = `${TipoEstado} - ${Fecha} · ${NumReservas} Reservas`;
+
   return (
-    <div>DayHeader</div>
+    <header className={IndicadorConEstado}>{Texto}</header>
   )
 }
