8f783ba8a64fb00e03b2d67dc14c313831f4dd2d
DonValen910 <valentinstito@gmail.com>
2026-01-31 17:06:45 -0300

    feat: agregar componente ReservationCard en la página DayPage

--- src/pages/days/DayPage.jsx
+++ src/pages/days/DayPage.jsx
@@ -1,12 +1,16 @@
 import DayHeader from "./DayHeader";
 import DayFooter from "./DayFooter";
 import BotonFiltro from "./components/BotonFiltro";
+import ReservationCard from "./components/ReservationCard";
 
 export default function DayPage() {
   return (
     <>
       <DayHeader TipoEstado="Terminado" Fecha="14, Dic" NumReservas={7} />
       <BotonFiltro />
+      <main>
+        <ReservationCard nombre={'Matias'} numCancha={1} seña={8000} horaInicio={"17:00"} horaFin={"18:00"} />
+      </main>
       <DayFooter />
     </>
   )
