df4be4c61b5fb5b7e4b67c0412bda6bb9ad13e09
DonValen910 <valentinstito@gmail.com>
2026-02-14 12:38:13 -0300

    feat: agregar componente InfoHeader para mostrar el estado de reservas

---
+++ src/components/InfoHeader.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { css } from '@styled-system/css'
+
+const IndicadorEstadoBase = css({
+    width: '100%',
+    height: '80px',
+    display: 'grid',
+    placeItems: 'center',
+    fontSize: '24px',
+    fontWeight: 'bold',
+    fontFamily: 'var(--fuente-montserrat)',
+})
+
+export default function InfoHeader({ TipoEstado, Fecha, NumReservas }) {
+    let estadoColor = '';
+
+    switch (TipoEstado) {
+        case "Pendiente":
+            estadoColor = css({
+                backgroundColor: 'var(--color-naranja)'
+            })
+            break;
+
+        case "Proximo":
+            estadoColor = css({
+                backgroundColor: 'var(--color-rojo)',
+            })
+            break;
+
+        case "Terminado":
+            estadoColor = css({
+                backgroundColor: 'var(--color-verde)',
+            })
+            break;
+
+        default:
+            console.log("Tipo de estado no reconocido");
+            break;
+    }
+
+    const IndicadorConEstado = `${IndicadorEstadoBase} ${estadoColor}`;
+    const Texto = `${TipoEstado} - ${Fecha} · ${NumReservas} Reservas`;
+
+    return (
+        <header className={IndicadorConEstado}>{Texto}</header>
+    )
+}
