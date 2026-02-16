d0c7362c4b89445a1cf4f4cc4c05dba6957a6dd8
DonValen910 <valentinstito@gmail.com>
2026-02-16 13:10:22 -0300

    feat: agregar componente Subtract para icono de resta

---
+++ src/components/icons/Subtract.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Subtract({ tamaño, color }) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={tamaño} height={tamaño} fill="none" viewBox="0 0 32 32"><path fill={color} d="M6.667 17.333v-2.666h18.666v2.666z" /></svg>
+    )
+}
+
+export default Subtract
