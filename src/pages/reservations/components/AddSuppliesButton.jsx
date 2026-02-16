9b679f35812995d92d044401a4c7e94471ec572d
DonValen910 <valentinstito@gmail.com>
2026-02-16 13:38:02 -0300

    feat: agregar manejador de clics al botón de agregar insumos

--- src/pages/reservations/components/AddSuppliesButton.jsx
+++ src/pages/reservations/components/AddSuppliesButton.jsx
@@ -3,24 +3,24 @@ import { css } from '@styled-system/css'
 import Plus from '@/components/icons/Plus'
 
 const ContenedorBoton = css({
-    display: 'flex',
-    alignItems: 'center',
-    gap: '5px',
-    fontFamily: 'var(--fuente-work-sans)',
-    fontSize: '22px',
-    padding: '8px 15px',
-    margin: '20px auto',
-    backgroundColor: 'var(--color-azul)',
-    borderRadius: '24px',
-    cursor: 'pointer',
-    transition: 'all 0.3s ease',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '5px',
+  fontFamily: 'var(--fuente-work-sans)',
+  fontSize: '22px',
+  padding: '8px 15px',
+  margin: '20px auto',
+  backgroundColor: 'var(--color-azul)',
+  borderRadius: '24px',
+  cursor: 'pointer',
+  transition: 'all 0.3s ease',
 })
 
-export default function AddSuppliesButton() {
+export default function AddSuppliesButton({ onClick }) {
   return (
-    <button className={ContenedorBoton}>
-        <figure><Plus color="var(--color-negro)" tamaño={42} /></figure>
-        Agregar insumos
+    <button className={ContenedorBoton} onClick={onClick}>
+      <figure><Plus color="var(--color-negro)" tamaño={42} /></figure>
+      Agregar insumos
     </button>
   )
 }
