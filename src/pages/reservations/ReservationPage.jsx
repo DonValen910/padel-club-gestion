92ff1d3727730f36e82d99437ad4a4c97b5bbe19
DonValen910 <valentinstito@gmail.com>
2026-02-16 13:37:54 -0300

    feat: agregar funcionalidad para mostrar y ocultar la lista de insumos en la página de reservas

--- src/pages/reservations/ReservationPage.jsx
+++ src/pages/reservations/ReservationPage.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { css } from '@styled-system/css'
 import InfoHeader from '@/components/InfoHeader';
 import Field from './components/Field';
 import ReservationFooter from './ReservationFooter';
+import AddSuppliesButton from './components/AddSuppliesButton';
+import ListSupplies from './components/ListSupplies';
 
 const ContenedorMain = css({
   width: '100%',
 })
 
 export default function ReservationPage() {
+  const [showSupplies, setShowSupplies] = useState(false);
+
   return (
     <>
       <InfoHeader TipoEstado="Terminado" Fecha="14, Dic" NumReservas={7} />
       <main className={ContenedorMain}>
         <Field />
+        <AddSuppliesButton onClick={() => setShowSupplies(!showSupplies)} />
+        {showSupplies && <ListSupplies />}
       </main>
       <ReservationFooter />
     </>
