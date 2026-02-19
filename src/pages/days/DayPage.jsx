3e16bb1097eb8e47f50d2c49f9d7a5512a86fb66
DonValen910 <valentinstito@gmail.com>
2026-02-19 16:32:05 -0300

    feat: agregar funcionalidad de filtro en la página de días

--- src/pages/days/DayPage.jsx
+++ src/pages/days/DayPage.jsx
@@ -1,22 +1,32 @@
+import { useState } from 'react';
 import { css } from '@styled-system/css'
 import DayFooter from "./DayFooter";
 import BotonFiltro from "./components/FilterButton";
 import ReservationCard from "./components/ReservationCard";
 import InfoHeader from '@/components/InfoHeader';
+import FilterCard from './components/FilterCard';
 
 const ContenedorCard = css({
     display: 'flex',
     flexDirection: 'column',
+    alignItems: 'center',
     gap: '1rem'
 })
 
 export default function DayPage() {
+    const [filtroActivo, setFiltroActivo] = useState(false);
+
+    const toggleFiltro = () => {
+        setFiltroActivo(!filtroActivo);
+    };
+
     return (
         <>
             <InfoHeader TipoEstado="Terminado" Fecha="14, Dic" NumReservas={7} />
-            <BotonFiltro />
+            <BotonFiltro activo={filtroActivo} toggleFiltro={toggleFiltro} />
             <main className={ContenedorCard}>
                 <ReservationCard nombre={'Matias'} numCancha={1} seña={8000} horaInicio={"17:00"} horaFin={"18:00"} />
+                {filtroActivo && <FilterCard />}
             </main>
             <DayFooter />
         </>
