ec8c41083cf453119b1c08eaad9352eaadb88e6c
DonValen910 <valentinstito@gmail.com>
2026-03-06 15:30:23 -0300

    feat: mejorar la capitalización del estado en el componente InfoHeader

--- src/components/InfoHeader.jsx
+++ src/components/InfoHeader.jsx
@@ -1,47 +1,47 @@
-import React from 'react'
-import { css } from '@styled-system/css'
+import React from "react";
+import { css } from "@styled-system/css";
 
 const IndicadorEstadoBase = css({
-    width: '100%',
-    height: '80px',
-    display: 'grid',
-    placeItems: 'center',
-    fontSize: '24px',
-    fontWeight: 'bold',
-    fontFamily: 'var(--fuente-montserrat)',
-})
+  width: "100%",
+  height: "80px",
+  display: "grid",
+  placeItems: "center",
+  fontSize: "24px",
+  fontWeight: "bold",
+  fontFamily: "var(--fuente-montserrat)",
+});
 
 export default function InfoHeader({ TipoEstado, Fecha, NumReservas }) {
-    let estadoColor = '';
+  const tipoEstadoCapitalizado =
+    TipoEstado.charAt(0).toUpperCase() + TipoEstado.slice(1).toLowerCase();
+  let estadoColor = "";
 
-    switch (TipoEstado) {
-        case "Pendiente":
-            estadoColor = css({
-                backgroundColor: 'var(--color-naranja)'
-            })
-            break;
+  switch (tipoEstadoCapitalizado) {
+    case "Pendiente":
+      estadoColor = css({
+        backgroundColor: "var(--color-naranja)",
+      });
+      break;
 
-        case "Proximo":
-            estadoColor = css({
-                backgroundColor: 'var(--color-rojo)',
-            })
-            break;
+    case "Proximo":
+      estadoColor = css({
+        backgroundColor: "var(--color-rojo)",
+      });
+      break;
 
-        case "Terminado":
-            estadoColor = css({
-                backgroundColor: 'var(--color-verde)',
-            })
-            break;
+    case "Terminado":
+      estadoColor = css({
+        backgroundColor: "var(--color-verde)",
+      });
+      break;
 
-        default:
-            console.log("Tipo de estado no reconocido");
-            break;
-    }
+    default:
+      console.log("Tipo de estado no reconocido");
+      break;
+  }
 
-    const IndicadorConEstado = `${IndicadorEstadoBase} ${estadoColor}`;
-    const Texto = `${TipoEstado} - ${Fecha} · ${NumReservas} Reservas`;
+  const IndicadorConEstado = `${IndicadorEstadoBase} ${estadoColor}`;
+  const Texto = `${tipoEstadoCapitalizado} - ${Fecha} · ${NumReservas} Reservas`;
 
-    return (
-        <header className={IndicadorConEstado}>{Texto}</header>
-    )
+  return <header className={IndicadorConEstado}>{Texto}</header>;
 }
